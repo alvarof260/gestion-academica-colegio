2f584d304793084912e870456a45847da2a32167
Alvaro Estanislao Figueroa <alvarof260@gmail.com>
2024-03-04 20:54:40 -0300

    feat: add Meet model

---
+++ app/api/src/models/meet.model.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose'
+
+const meetCollection = 'meets'
+
+const meetSchema = new mongoose.Schema({
+  to: {
+    type: String,
+    required: true
+  },
+  from: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true,
+    default: Date.now
+  },
+  assist: {
+    type: String,
+    enum: ['YES', 'NO', 'PENDING'],
+    default: 'PENDING'
+  },
+  expiration: {
+    type: Date,
+    required: true
+  }
+})
+
+export const Meet = mongoose.model(meetCollection, meetSchema)
